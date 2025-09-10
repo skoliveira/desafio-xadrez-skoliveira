0a9358d9daf86a4e2a162f1889ea002cc5afe745
Samuel König de Oliveira <skoliveira@users.noreply.github.com>
2025-09-10 01:26:32 +0000

    Implementação das funções recursivas para movimentação do Bispo, Torre e Rainha no jogo de xadrez

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,27 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void moverBispo (int num) {
+    if (num > 0) {
+        moverBispo (num-1);
+        printf("%d. Cima, Direita\n", num);
+    }
+}
+
+void moverTorre (int num) {
+    if (num > 0) {
+        moverTorre (num-1);
+        printf("%d. Direita\n", num);
+    }
+}
+
+void moverRainha (int num) {
+    if (num > 0) {
+        moverRainha (num-1);
+        printf("%d. Esquerda\n", num);
+    }
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -29,42 +50,59 @@ int main() {
     // Inclua o uso de continue e break dentro dos loops.
     
     int movimento_bispo = 5;
-    printf("Movimentação do Bispo:\n");
+    printf("Movimentação do Bispo (recursiva):\n");
+    moverBispo(movimento_bispo);
+
+    printf("\nMovimentação do Bispo (loop alinhados):\n");
     for (int i = 0; i < movimento_bispo; i++)
     {
-        printf("%d. Cima, Direita\n", i+1);
+        printf("%d. Cima", i+1);
+        for (int j = 0; j < movimento_bispo; j++)
+        {
+            if(i == j) {
+                printf(", Direita\n", j+1);
+            }
+        }
     }
+    
     
     int movimento_torre = 5;
     printf("\nMovimentação da Torre:\n");
-    int j = 0;
-    while (j < movimento_torre)
-    {   
-        printf("%d. Direita\n", j+1);
-        j++;
-    }
+    moverTorre(movimento_torre);
     
     int movimento_rainha = 8;
     printf("\nMovimentação da Rainha:\n");
-    int k = 0;
-    do
-    {
-        printf("%d. Esquerda\n", k+1);
-        k++;
-    } while (k < movimento_rainha);
-
-    int movimento_cavalo[2] = {1,2};
+    moverRainha(movimento_rainha);
+  
+    // Implementação aprimorada do movimento do Cavalo
+    const int MOVIMENTO_VERTICAL = 2;
+    const int MOVIMENTO_HORIZONTAL = 1;
+    int posicao_atual = 1;
     printf("\nMovimentação do Cavalo:\n");
-    for (int l = 0; l < movimento_cavalo[0]; l++)
-    {
-        int m = 0;
-        while (m < movimento_cavalo[1])
-        {
-            printf("%d. Baixo\n", m+1);
-            m++;
+    for (int y = 1; y <= MOVIMENTO_VERTICAL + MOVIMENTO_HORIZONTAL; y++) {
+        for (int x = 1; x <= 2; x++) {
+            // Pula iterações desnecessárias
+            if (y > MOVIMENTO_VERTICAL && x > 1) {
+                continue;
+            }
+            
+            // Controle do movimento vertical
+            if (y <= MOVIMENTO_VERTICAL) {
+                printf("%d. Cima\n", posicao_atual++);
+                // Interrompe o loop interno após movimento vertical
+                break;
+            }
+            
+            // Controle do movimento horizontal
+            if (y > MOVIMENTO_VERTICAL) {
+                printf("%d. Direita\n", posicao_atual++);
+                // Finaliza após o movimento horizontal
+                if (x == MOVIMENTO_HORIZONTAL) {
+                    break;
+                }
+            }
         }
-        printf("%d. Esquerda\n", m+1);
     }
-    
+
     return 0;
 }
